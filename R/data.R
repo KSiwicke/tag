6b28c4da39b5c59bc05f8a8fd52b462b449a23c8
Kevin Siwicke <kevin.siwicke@noaa.gov>
2023-03-21 09:30:20 -0800

    added data doc

--- R/data.R
+++ R/data.R
@@ -1,13 +1,9 @@
 #' Example data
 #'
-#' An example likelihood from a Pacific cod with associated bathymetry file
+#' An example likelihood from a Pacific cod in the Aleutian Islands
 #'
 #' @format null An array with likelihoods in each cell of the grid for each day
 
 #' @rdname L
 #' @format NULL
 "L"
-
-#' @rdname bathy.maps.3k
-#' @format NULL
-"bathy.maps.3k"
